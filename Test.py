291e1934551c7de31c5a2f7ec97b10fefe797923
Western State Colorado University Computer Science Department <lucas.vandenbergh@western.edu>
2016-03-15 15:22:33 -0600

    Update Test.py

--- Test.py
+++ Test.py
@@ -4,6 +4,7 @@ from pythonfrp import *
 from pythonfrp.Numerics import *
  
 
+
 #Square(p2(25,25))
 #circle(integral(mouse-p2(100,100),p2(100,100)),time*10)
 
@@ -17,7 +18,6 @@ bant = circle(position = p2(10,50 - time),size = 10)
 #Square(paddle[0], size = 50, skew = 0.2)
 
 #Triangle(integral(mouse-p2(100,100),p2(100,100)))
-
 
 #def makeSquare(m,v):
 #    square(p2(localTime*50+v.x,localTime*50+v.y))
@@ -36,6 +36,7 @@ def keyPressed(self, k):
  
  #react(kT,typeKey)
  
+
 #react(kT(), keyPressed)
 
 start()
